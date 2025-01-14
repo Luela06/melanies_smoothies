e6df98d9c919a6fe54a261f2fea36034b3776294
Luela06 <palaypayonluelajoy86@gmail.com>
2025-01-14 09:13:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
+import requests
 from snowflake.snowpark.functions import col
  
 # Write directly to the app
@@ -15,8 +16,14 @@ st.write("The name on your Smoothie will be:", name_on_order)
  
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col ('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+ 
+#Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df=my_dataframe.to_pandas()
+#st.dataframe(pd_df)
+#st.stop()
  
 ingredients_List = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -24,12 +31,19 @@ ingredients_List = st.multiselect(
     , max_selections=5
 )
 if ingredients_List:
-
     ingredients_string = ''
 
  
     for fruit_chosen in ingredients_List:
         ingredients_string += fruit_chosen + ' '
+ 
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+ 
+        st.subheader(fruit_chosen + " " + 'Nutrition Informatio')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+ 
  
     #st.write(ingredients_string)
  
@@ -48,7 +62,3 @@ if ingredients_List:
     #if ingredients_string:
         #session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅",)
- 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
